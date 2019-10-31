58daa6ba839009f6d751bdf11975be0831326f87
aleksandra sagitova <aleksandrasagitova@aleksandras-MacBook-Pro.local>
2019-10-31 10:50:55 +0100

    y

--- js.js
+++ js.js
@@ -8,3 +8,20 @@ for (let i = 0; i < buttons.length; i++) {
         display.textContent = value;
     }
 }
+
+
+function calc() {
+    let n1 = parseInt(document.getElementById)('n1').value;
+    let n2 = parseInt(document.getElementById(n2)).value;
+    let oper = document.getElementById('operations').value;
+
+
+    if (oper === '+') {
+        let n2 = document.getElementById('result').value = n1 + n2;
+
+
+
+
+
+    }
+}
